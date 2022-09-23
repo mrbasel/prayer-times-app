6dd1ff8698aeba73b1e242ba8773032e28199d25
Basel <mrbasel555@gmail.com>
2022-09-23 05:08:20 +0300

    add switch lang links

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,7 +6,15 @@ export const Footer = () => {
   const { t } = useTranslation("index");
   return (
     <footer className={styles.footer}>
-      <p>
+      <div className={styles.langContainer}>
+        <Link href="/" locale="en" key="en">
+          <a>English</a>
+        </Link>
+        <Link href="/" locale="ar" key="ar">
+          <a>العربية</a>
+        </Link>
+      </div>
+      <div>
         {t("made-by")}{" "}
         <Link href="https://www.baselalnassr.com/">
           <a>{t("basel")}</a>
@@ -15,7 +23,7 @@ export const Footer = () => {
         <Link href="https://github.com/mrbasel">
           <a>{t("source")}</a>
         </Link>
-      </p>
+      </div>
     </footer>
   );
 };
